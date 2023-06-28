39a729a95b1f9e501e107a8395039445a782a2bd
HDef <35143098+TheChaman@users.noreply.github.com>
2023-06-28 18:43:20 +0200

    Algorithme Resolution

--- src/main/java/sudoku/Resolution.java
+++ src/main/java/sudoku/Resolution.java
@@ -1,5 +1,10 @@
 package sudoku;
 import java.util.Set;
+
+import sudoku.Exceptions.ElementInterditException;
+import sudoku.Exceptions.HorsBornesException;
+import sudoku.Exceptions.ValeurImpossibleException;
+import sudoku.Exceptions.ValeurInitialeModificationException;
 /**
  * Interface de résolveur de Grille
  *
@@ -12,16 +17,16 @@ public class Resolution implements Solveur {
      * @param grille Grille à résoudre
      * @return true si la grille a été résolue
      */
-    public final boolean solve(final Grille grille) {
+    public final boolean solve(final Grille grille) throws HorsBornesException, ElementInterditException, ValeurInitialeModificationException, ValeurImpossibleException {
         System.out.println(grille);
 
-        Set<ElementDeGrille> elementsvalides = grille.getElements();
+        Set<ElementDeGrille> elementsValides = grille.getElements();
 
         boolean mouvementPossible = false;
 
         int nombreLignes = grille.getDimension();
         int nombreCol = nombreLignes;
-        
+
         // Si la grille de Sudoku est complète, on arrête le programme puisque le sudoku est résolu
         if(grille.isComplete()) {
             return true;
@@ -29,7 +34,16 @@ public class Resolution implements Solveur {
 
         for(int i = 0; i < nombreLignes; i++) {
             for(int j = 0; j < nombreCol; j++) {
-                
+                for(ElementDeGrille caseATester: elementsValides) {
+                    mouvementPossible = grille.isPossible(i, j, caseATester);
+                    ElementDeGrille valeurASaisir = grille.getValue(i, j);
+
+                    if(mouvementPossible && valeurASaisir == null) {
+                        grille.setValue(i, j, caseATester);
+                    } else {
+                        grille.setValue(i, j, null);
+                    }
+                }
             }
         }
 
--- src/main/java/sudoku/Solveur.java
+++ src/main/java/sudoku/Solveur.java
@@ -1,5 +1,10 @@
 package sudoku;
 
+import sudoku.Exceptions.ElementInterditException;
+import sudoku.Exceptions.HorsBornesException;
+import sudoku.Exceptions.ValeurImpossibleException;
+import sudoku.Exceptions.ValeurInitialeModificationException;
+
 /**
  * Interface de résolveur de Grille
  *
@@ -12,5 +17,5 @@ public interface Solveur {
      * @param grille Grille à résoudre
      * @return true si la grille a été résolue
      */
-    boolean solve(Grille grille);
+    boolean solve(Grille grille) throws HorsBornesException, ElementInterditException, ValeurInitialeModificationException, ValeurImpossibleException;
 }
